e885d82c3c740517c34eb4fc244e21d14e815fff
NATHAN NICHOLS <rl235817@rhs-macair-40.local>
2018-12-13 10:53:13 -0600

    Initial Commit

---
+++ MakeMeBlue/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MakeMeBlue
+//
+//  Created by NATHAN NICHOLS on 8/31/18.
+//  Copyright © 2018 NATHAN NICHOLS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var makeMeBlueLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       makeMeBlueLabel.textColor = .blue
+    }
+
+   
+
+}
+
